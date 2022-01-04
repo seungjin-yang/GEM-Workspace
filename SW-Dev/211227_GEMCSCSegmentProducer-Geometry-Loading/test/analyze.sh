cdb457d93ba439ff55fbf00d6eb8b88ea079176b
Seungjin Yang <seungjin.yang@cern.ch>
2022-01-04 14:03:43 +0900

    update

--- SW-Dev/211227_GEMCSCSegmentProducer-Geometry-Loading/test/analyze.sh
+++ SW-Dev/211227_GEMCSCSegmentProducer-Geometry-Loading/test/analyze.sh
@@ -2,7 +2,8 @@
 CFG=runGEMCSCSegmentAnalyzer_cfg.py
 INPUT_FILE=./step3_numEvent1000.root
 FILE_PREPEND=file:
+LOG_FILE="analysis.txt"
 
 COMMAND="cmsRun ${CFG} inputFiles=${INPUT_FILE} filePrepend=${FILE_PREPEND}"
 echo ${COMMAND}
-eval ${COMMAND}
+eval ${COMMAND} > ${LOG_FILE} 2>&1 &
