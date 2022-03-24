738d7ce8577e6969fe59fc77811dd3dee7298219
Seungjin Yang <seungjin.yang@cern.ch>
2022-03-24 18:22:27 +0900

    update

--- SW-Dev/211227_Migration-to-EventSetup-with-ESGetToken-in-GEMCSCSegmentProducer/test/CMSSW_12_2_X_2022-03-18-2300/11611.0_TenMuExtendedE_0_200+2021/test-runGEMCSCSegmentAnalyzer.sh
+++ SW-Dev/211227_Migration-to-EventSetup-with-ESGetToken-in-GEMCSCSegmentProducer/test/CMSSW_12_2_X_2022-03-18-2300/11611.0_TenMuExtendedE_0_200+2021/test-runGEMCSCSegmentAnalyzer.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 CFG_FILE=runGEMCSCSegmentAnalyzer_cfg.py
+INPUT_FILE=step3.root
+FILE_PREPEND=file:
 
 NAME=$(basename ${CFG_FILE})
 NAME=${NAME/".py"/""}
-
 LOG_FILE=./logs/test-${NAME}.log
 
-cmsRun ${CFG_FILE} > ${LOG_FILE} 2>&1 &
+cmsRun ${CFG_FILE} inputFiles=${INPUT_FILE} filePrepend=${FILE_PREPEND} > ${LOG_FILE} 2>&1 &
 tail -f ${LOG_FILE}
