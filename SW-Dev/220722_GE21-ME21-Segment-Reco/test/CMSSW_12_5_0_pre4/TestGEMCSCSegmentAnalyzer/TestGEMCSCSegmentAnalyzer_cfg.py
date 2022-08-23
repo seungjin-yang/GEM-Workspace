a17edc0b204088a077323ffa2a9677605cc49a26
Seungjin Yang <slowmoyang@gmail.com>
2022-08-23 20:37:15 +0900

    update

--- SW-Dev/220722_GE21-ME21-Segment-Reco/test/CMSSW_12_5_0_pre4/TestGEMCSCSegmentAnalyzer/TestGEMCSCSegmentAnalyzer_cfg.py
+++ SW-Dev/220722_GE21-ME21-Segment-Reco/test/CMSSW_12_5_0_pre4/TestGEMCSCSegmentAnalyzer/TestGEMCSCSegmentAnalyzer_cfg.py
@@ -1,6 +1,6 @@
-import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms # type: ignore
 
-from Configuration.Eras.Era_Run3_cff import Run3
+from Configuration.Eras.Era_Run3_cff import Run3 # type: ignore
 
 process = cms.Process("TestGEMCSCSegment", Run3)
 
@@ -9,24 +9,27 @@ process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
-from Configuration.AlCa.GlobalTag import GlobalTag
+from Configuration.AlCa.GlobalTag import GlobalTag # type: ignore
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2022_realistic', '')
 
-from FWCore.ParameterSet.VarParsing import VarParsing
-options = VarParsing('analysis')
-options.parseArguments()
-
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(options.maxEvents)
+    input = cms.untracked.int32(-1)
 )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(options.inputFiles)
+    fileNames = cms.untracked.vstring()
 )
 
 process.gemcscs = cms.EDAnalyzer('TestGEMCSCSegmentAnalyzer',
-    RootFileName = cms.untracked.string(options.outputFile),
+    RootFileName = cms.untracked.string('file:output.root'),
     Debug = cms.untracked.bool(True),
 )
 
 process.p = cms.Path(process.gemcscs)
+
+from FWCore.ParameterSet.VarParsing import VarParsing # type: ignore
+options = VarParsing('analysis')
+options.parseArguments()
+process.maxEvents.input = options.maxEvents
+process.source.fileNames = options.inputFiles
+process.gemcscs.RootFileName = options.outputFile
