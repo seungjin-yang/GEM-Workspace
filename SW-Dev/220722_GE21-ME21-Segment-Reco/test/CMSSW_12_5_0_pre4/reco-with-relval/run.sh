68a5f73047b5402b52426251ab6c1ade1088e95e
Seungjin Yang <slowmoyang@gmail.com>
2022-08-28 07:59:59 +0200

    update

--- SW-Dev/220722_GE21-ME21-Segment-Reco/test/CMSSW_12_5_0_pre4/reco-with-relval/run.sh
+++ SW-Dev/220722_GE21-ME21-Segment-Reco/test/CMSSW_12_5_0_pre4/reco-with-relval/run.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
+###############################################################################
+# tools
+###############################################################################
+function checkvar() {
+    VAR=${1}
+    echo "${VAR}=${!VAR}"
+}
+
+###############################################################################
+# print info
+###############################################################################
+echo "start: $(date)"
 echo "hostname: $(hostname)"
+echo "pwd: $(pwd)"
+ls -lha ./
 
+###############################################################################
+# arguments
+###############################################################################
 input_file=${1}
-echo "input_file=${input_file}"
+output_file=${2}
 
-pwd
+checkvar input_file
+checkvar output_file
 
+###############################################################################
+# setup
+###############################################################################
 CMSSW_VERSION=CMSSW_12_5_0_pre4
-
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsrel ${CMSSW_VERSION}
 cd ${CMSSW_VERSION}/src
@@ -16,26 +36,24 @@ git-cms-merge-topic slowmoyang:ge21-me21-seg-reco__from-CMSSW_12_5_0_pre4
 scram b
 cd -
 
-#source /cvmfs/cms.cern.ch/cmsset_default.sh
-#cd /pad/seyang/Projects/GEM-Workspace/SW-Dev/220722_GE21-ME21-Segment-Reco/CMSSW_12_5_0_pre4/
-#eval `scramv1 runtime -sh`
-#cd -
+checkvar SCRAM_ARCH
+checkvar CMSSW_VERSION
+checkvar CMSSW_BASE
 
-echo "SCRAM_ARCH=${SCRAM_ARCH}"
-echo "CMSSW_VERSION=${CMSSW_VERSION}"
-echo "CMSSW_BASE=${CMSSW_BASE}"
-
+###############################################################################
+# run
+###############################################################################
 cfg_file=step2_RAW2DIGI_L1Reco_RECO_GEMCSCSegment.py
 file_prepend="root://eoscms.cern.ch//eos/cms"
-output_file="output.root"
 
 cmsRun ${cfg_file} \
     inputFiles=${input_file} \
     filePrepend=${file_prepend} \
     outputFile=${output_file}
 
-ls -lha .
-
+###############################################################################
+# report
+###############################################################################
 stat ${output_file}
 
-# hdfs dfs -put ${output_file} /user/seyang/GEM/SW-Dev/220722_GE21-ME21-Segment-Reco/test/CMSSW_12_5_0_pre4/reco-with-relval
+echo "end: $(date)"
